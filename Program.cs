4266b35c41b2204e83e264e86b8c3390c6a56991
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-10-24 20:26:18 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            
+            float.TryParse(Console.ReadLine(), out float v1);   
+            Console.Write("Segundo valor: ");
+            float.TryParse(Console.ReadLine(), out float v2);   
+
+            Console.WriteLine("");  
+
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da: {resultado}");          
+        }
+    }
+}
+
