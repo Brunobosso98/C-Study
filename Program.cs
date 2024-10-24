4120c470364ae7621448d2b989992c8a62798e65
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-10-24 20:32:52 -0300

    adicionada subtracao

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+         Subtracao();
+        }
+        static void Soma()
+        {
             Console.Clear();
             Console.Write("Primeiro valor: ");
             
@@ -16,7 +20,25 @@ namespace Calculator
             Console.WriteLine("");  
 
             float resultado = v1 + v2;
-            Console.WriteLine($"O resultado da: {resultado}");          
+            Console.WriteLine($"O resultado da: {resultado}"); 
+            Console.ReadKey();
+
+        }
+        static void Subtracao() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float.TryParse(Console.ReadLine(), out float v1);   
+            
+            Console.WriteLine("Segundo valor: ");
+            float.TryParse(Console.ReadLine(), out float v2);
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
         }
     }
 }
